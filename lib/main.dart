a19f2c0a4aca6d8fa149c9182e41956f0658ec7e
0000MAILY1111 <valdezmelizza19@gmail.com>
2024-12-07 15:09:40 -0400

    autcontrolegrlogin

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       title: 'Web Services',
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+      home: MyHomePage(),    //se añadio el const para que no se vuelva a cargar la pagina
     );
   }
 }
